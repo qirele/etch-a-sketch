9a35726a871b1395f3ebec53a1141ccf7eb93346
qirele <kirilczikzik@gmail.com>
2022-11-01 13:48:31 +0100

    add coloring to the individual etch a sketch tiles

--- main.js
+++ main.js
@@ -1,14 +1,52 @@
 const wrapper = document.querySelector(".wrapper");
+const btn = document.querySelector("button");
 
+btn.addEventListener("click", () => {
+  let num;
 
-for (let i = 0; i < 256; i++) {
-  const div = document.createElement("div");
-  div.classList.add("item");
-  wrapper.appendChild(div);
-  div.addEventListener("mouseover", changeColor);
+  num = prompt("Gimme a number less than 100", "");
+
+  if (isNaN(num) || num === null || num === "") {
+    num = 16;
+  } else {
+    num = parseInt(num);
+  }
+
+  if (num < 0) {
+    num = 2;
+  }
+
+  if (num > 100) {
+    num = 100;
+  }
+
+  while (wrapper.firstChild) wrapper.removeChild(wrapper.lastChild);
+  createGrid(num);
+});
+
+createGrid(16);
+
+function createGrid(num) {
+  for (let i = 0; i < num * num; i++) {
+    const div = document.createElement("div");
+    div.classList.add("item");
+    wrapper.appendChild(div);
+
+    wrapper.style.gridTemplateColumns = `repeat(${num}, ${800 / num}px)`;
+    wrapper.style.gridTemplateRows = `repeat(${num}, ${800 / num}px)`;
+    div.addEventListener("mouseover", changeColor);
+  }
 }
+
+let lightness = 100;
 
 function changeColor() {
-  this.style.background = "gray";
-}
+  let hue = Math.floor(Math.random() * 360);
+  this.style.background = `hsl(${hue}, 100%, ${lightness}%)`;
 
+  lightness -= 10;
+
+  if (lightness === -10) {
+    lightness = 100;
+  }
+}
